238e7b4c5a488dbc21033194f8e2123fb1fb37c7
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2024-07-03 17:26:56 -0500

    Timeout increased when running e2e tests

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -77,5 +77,6 @@ module.exports = defineConfig({
       'cd ../backend && npm install && npm run build-ui && npm run start:test',
     url: 'http://localhost:3003',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000,
   },
 })
