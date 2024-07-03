28f304b1b3989fe9c7cd3aac9b4e82dcac5456c7
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2024-07-03 17:12:28 -0500

    Commands added for e2e tests

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -73,7 +73,8 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd ../backend && npm run start:test',
+    command:
+      'cd ../backend && npm install && npm run build-ui && npm run start:test',
     url: 'http://localhost:3003',
     reuseExistingServer: !process.env.CI,
   },
