f0fa19f66812b9ad70fdba14dcf1cb9f4a5e9192
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2024-07-03 19:02:23 -0500

    Playwright configuration updated

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -11,16 +11,15 @@ const { defineConfig, devices } = require('@playwright/test')
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  timeout: 10000,
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -77,6 +76,5 @@ module.exports = defineConfig({
       'cd ../backend && npm install && npm run build-ui && npm run start:test',
     url: 'http://localhost:3003',
     reuseExistingServer: !process.env.CI,
-    timeout: 600000,
   },
 })
--- e2e/tests/bloglist_app.spec.js
+++ e2e/tests/bloglist_app.spec.js
@@ -18,107 +18,107 @@ describe('Blog app', () => {
     await expect(page.getByText('log in to application')).toBeVisible()
   })
 
-  // describe('Login', () => {
-  //   test('succeeds with correct credentials', async ({ page }) => {
-  //     await loginWith(page, 'Nick220505', '123')
-  //     await expect(page.getByText('Nicolas Pardo logged in')).toBeVisible()
-  //     await expect(page.getByText('Blog App')).toBeVisible()
-  //   })
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'Nick220505', '123')
+      await expect(page.getByText('Nicolas Pardo logged in')).toBeVisible()
+      await expect(page.getByText('Blog App')).toBeVisible()
+    })
 
-  //   test('fails with wrong credentials', async ({ page }) => {
-  //     await loginWith(page, 'Nick220505', 'wrongPassword')
-  //     await expect(page.getByRole('alert')).toHaveText(
-  //       'wrong username or password'
-  //     )
-  //     await expect(page.getByText('Nicolas Pardo logged in')).not.toBeVisible()
-  //   })
-  // })
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'Nick220505', 'wrongPassword')
+      await expect(page.getByRole('alert')).toHaveText(
+        'wrong username or password'
+      )
+      await expect(page.getByText('Nicolas Pardo logged in')).not.toBeVisible()
+    })
+  })
 
-  // describe('When logged in', () => {
-  //   beforeEach(async ({ page }) => {
-  //     await loginWith(page, 'Nick220505', '123')
-  //   })
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'Nick220505', '123')
+    })
 
-  //   test('a new blog can be created', async ({ page }) => {
-  //     await createBlog(
-  //       page,
-  //       'This is a new blog',
-  //       'Harry Potter',
-  //       'http://example-blog.com'
-  //     )
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(
+        page,
+        'This is a new blog',
+        'Harry Potter',
+        'http://example-blog.com'
+      )
 
-  //     await expect(page.getByRole('alert')).toHaveText(
-  //       'blog This is a new blog by Harry Potter has been added'
-  //     )
-  //     await expect(
-  //       page.getByRole('link', { name: 'This is a new blog' })
-  //     ).toBeVisible()
-  //   })
+      await expect(page.getByRole('alert')).toHaveText(
+        'blog This is a new blog by Harry Potter has been added'
+      )
+      await expect(
+        page.getByRole('link', { name: 'This is a new blog' })
+      ).toBeVisible()
+    })
 
-  //   test('blog can be liked', async ({ page }) => {
-  //     await createBlog(
-  //       page,
-  //       'This is a new blog',
-  //       'Harry Potter',
-  //       'http://example-blog.com'
-  //     )
-  //     await page.getByRole('link', { name: 'This is a new blog' }).click()
+    test('blog can be liked', async ({ page }) => {
+      await createBlog(
+        page,
+        'This is a new blog',
+        'Harry Potter',
+        'http://example-blog.com'
+      )
+      await page.getByRole('link', { name: 'This is a new blog' }).click()
 
-  //     await expect(page.getByText('0 likes')).toBeVisible()
+      await expect(page.getByText('0 likes')).toBeVisible()
 
-  //     await page.getByRole('button', { name: 'Like' }).click()
+      await page.getByRole('button', { name: 'Like' }).click()
 
-  //     await expect(page.getByRole('alert')).toHaveText(
-  //       'blog This is a new blog by Harry Potter has been updated'
-  //     )
-  //     await expect(page.getByText('1 likes')).toBeVisible()
-  //   })
+      await expect(page.getByRole('alert')).toHaveText(
+        'blog This is a new blog by Harry Potter has been updated'
+      )
+      await expect(page.getByText('1 likes')).toBeVisible()
+    })
 
-  //   test('blog can be deleted by the user who added it', async ({ page }) => {
-  //     await createBlog(
-  //       page,
-  //       'This is a new blog',
-  //       'Harry Potter',
-  //       'http://example-blog.com'
-  //     )
-  //     await page.getByRole('link', { name: 'This is a new blog' }).click()
-  //     await page.getByRole('button', { name: 'Remove' }).click()
-  //     await expect(page.getByRole('alert')).toHaveText(
-  //       'blog This is a new blog by Harry Potter has been removed'
-  //     )
-  //     await expect(
-  //       page.getByRole('link', { name: 'This is a new blog' })
-  //     ).not.toBeVisible()
-  //   })
+    test('blog can be deleted by the user who added it', async ({ page }) => {
+      await createBlog(
+        page,
+        'This is a new blog',
+        'Harry Potter',
+        'http://example-blog.com'
+      )
+      await page.getByRole('link', { name: 'This is a new blog' }).click()
+      await page.getByRole('button', { name: 'Remove' }).click()
+      await expect(page.getByRole('alert')).toHaveText(
+        'blog This is a new blog by Harry Potter has been removed'
+      )
+      await expect(
+        page.getByRole('link', { name: 'This is a new blog' })
+      ).not.toBeVisible()
+    })
 
-  //   test("only the user who added the blog sees the blog's delete button", async ({
-  //     page,
-  //     request,
-  //   }) => {
-  //     await createBlog(
-  //       page,
-  //       'This is a new blog',
-  //       'Harry Potter',
-  //       'http://example-blog.com'
-  //     )
-  //     await page.getByRole('link', { name: 'This is a new blog' }).click()
-  //     await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible()
-  //     await page.getByRole('button', { name: 'Logout' }).click()
-  //     await request.post('/api/users', {
-  //       data: {
-  //         username: 'newUser',
-  //         password: '456',
-  //         name: 'Another user',
-  //       },
-  //     })
-  //     await loginWith(page, 'newUser', '456')
-  //     await expect(page.getByText('Blog App')).toBeVisible()
-  //     await expect(page.getByText('Another user logged in')).toBeVisible()
-  //     await page.goto('/')
-  //     await page.getByRole('link', { name: 'This is a new blog' }).click()
-  //     await expect(
-  //       page.getByRole('button', { name: 'Remove' })
-  //     ).not.toBeVisible()
-  //   })
+    test("only the user who added the blog sees the blog's delete button", async ({
+      page,
+      request,
+    }) => {
+      await createBlog(
+        page,
+        'This is a new blog',
+        'Harry Potter',
+        'http://example-blog.com'
+      )
+      await page.getByRole('link', { name: 'This is a new blog' }).click()
+      await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible()
+      await page.getByRole('button', { name: 'Logout' }).click()
+      await request.post('/api/users', {
+        data: {
+          username: 'newUser',
+          password: '456',
+          name: 'Another user',
+        },
+      })
+      await loginWith(page, 'newUser', '456')
+      await expect(page.getByText('Blog App')).toBeVisible()
+      await expect(page.getByText('Another user logged in')).toBeVisible()
+      await page.goto('/')
+      await page.getByRole('link', { name: 'This is a new blog' }).click()
+      await expect(
+        page.getByRole('button', { name: 'Remove' })
+      ).not.toBeVisible()
+    })
   })
 })
